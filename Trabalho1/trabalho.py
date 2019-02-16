335db77f5f885d486633d9e1f263357d3e2fb6f6
Thiago Almeida <thiago9863@gmail.com>
2019-02-16 13:06:54 -0200

    Matriz Euler Estático e Diferença Central

--- Trabalho1/trabalho.py
+++ Trabalho1/trabalho.py
@@ -6,19 +6,70 @@ Created on Fri Feb 15 14:49:23 2019
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-n = 3
-N = n**2
-diagonal_principal = np.ones((n,), np.float64)*1
-diagonal_r = np.ones((n-1,), np.float64)*2
-diagonal_p = np.ones((n,), np.float64)*3
+#Definicao de valores
+u = 1.0 #vento leste-oeste
+v = 1.0 #vento sul-norte
+h = 0.5 #espacamento da grade
 
-T = np.diagflat(diagonal_principal, 0) + np.diagflat(diagonal_r, 1)
-I = np.diagflat(diagonal_p, 0)
-print(T)
-print("------")
-print(I)
-#print("------")
-#print(A+B)
+def matrizEulerEstatico(n, u, v, h):
+    
+    p = 1#(u-v)/h
+    q = 3#-u/h
+    r = 2#v/h
+    
+    diagonal_principal = np.ones((n,), np.float64) * p
+    diagonal_r = np.ones((n-1,), np.float64) * r
+    diagonal_p = np.ones((n,), np.float64) * q
+    
+    T = np.diagflat(diagonal_principal, 0) + np.diagflat(diagonal_r, 1)
+    I = np.diagflat(diagonal_p, 0)
+    
+    diagonal_k = np.ones((n-1,), np.float64)
+    K = np.diagflat(diagonal_k, -1)
+    
+    A = np.kron(np.identity(n), T) + np.kron(K, I)
 
-#A = 
+    
+    print(T)
+    print("------")
+    print(I)
+    print("------")
+    print(K)
+    print("------")
+    print(A)
+    
+    return A
+
+def matrizDiferencaCentral(n, u, v, h):
+    
+    p = 1
+    q = 3
+    r = 2
+    
+    diagonal_principal = np.ones((n,), np.float64) * p
+    diagonal_r = np.ones((n-1,), np.float64) * r
+    diagonal_p = np.ones((n,), np.float64) * q
+    
+    T = np.diagflat(diagonal_principal, 0) + np.diagflat(diagonal_r, 1) + np.diagflat(diagonal_r, -1) * -1
+    I = np.diagflat(diagonal_p, 0)
+    
+    diagonal_k = np.ones((n-1,), np.float64)
+    K = np.diagflat(diagonal_k, -1) * -1 + np.diagflat(diagonal_k, 1)
+    
+    A = np.kron(np.identity(n), T) + np.kron(K, I)
+
+    
+    print(T)
+    print("------")
+    print(I)
+    print("------")
+    print(K)
+    print("------")
+    print(A)
+    
+    return A   
+
+matriz = matrizEulerEstatico(3, u, v, h)
+#matriz = matrizDiferencaCentral(3, u, v, h)
